5b03eba80087f49cc2f6589a09d6e43b1aab04d2
Leonardo George <leonardogeorge30@gmail.com>
2025-05-07 06:06:38 -0300

    Conceito de Variaveis

--- Hello-World/index.php
+++ Hello-World/index.php
@@ -1,4 +1,8 @@
 
 <?php
-    echo "Helo World!";
+    $feitoPor = "Feito por Leonardo George";
+
+    // This is a simple PHP script that outputs "Hello World!" to the browser
+    echo "<h1>Helo World!</h1> <br/>";
+    echo $feitoPor;
 ?>
