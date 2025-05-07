23bb16d7cdca30d7de9b312be583574a79cb8bd1
Leonardo George <leonardogeorge30@gmail.com>
2025-05-07 07:22:36 -0300

    modifications

---
+++ 04. Strings no PHP/strings.php
@@ -0,0 +1,13 @@
+<?php
+// Strings in PHP...
+
+// Exemplo de string simples
+    $nome = 'João';
+    //echo "Nome: $nome\n"; // Saída: Nome: João
+// Concatenação de strings
+    $sobrenome = 'Silva';
+    $nomeCompleto = $nome . ' ' . $sobrenome;
+    echo "Nome completo: $nomeCompleto\n"; // Saída: Nome completo: João Silva
+
+
+?>
