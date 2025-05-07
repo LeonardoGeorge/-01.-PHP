c8642c4c87c0cc69d08a2cc9438e02d5c54dac62
Leonardo George <leonardogeorge30@gmail.com>
2025-05-07 06:13:57 -0300

    Comentarios

--- Hello-World/index.php
+++ Hello-World/index.php
@@ -1,3 +1,16 @@
+/*
+    - Toda a variavel em PHP começa com o simbolo $.
+    - As variaveis podem conter:
+     -- Letras
+     -- Numeros 
+     -- Simbolo de underscore (_).
+     -- As variaveis não podem começar com numeros.
+     -- As variaveis são case-sensitive, ou seja: 
+        --$nome e $Nome são variaveis diferentes.    
+*/
+
+
+
 
 <?php
     $feitoPor = "Feito por Leonardo George";
