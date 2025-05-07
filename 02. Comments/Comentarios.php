ec10e5e3424292dc0d58108c143e9df839282971
Leonardo George <leonardogeorge30@gmail.com>
2025-05-07 06:52:33 -0300

    finished

---
+++ 02. Comments/Comentarios.php
@@ -0,0 +1,30 @@
+<?php
+// Usar camelCase para nombres de variables y funciones
+    $nombre = "Juan";   
+// Evite nomes generales como $a, $b, $c
+    $edad = 25; 
+//Escolha nomes de variaveis que descrevam o que elas representam
+    $altura = 1.75;
+// Evite nomes de variaveis que sejam muito longos ou complicados
+    $nomeDoUsuarioLogado = 'joaodasilva123'; // É Ruim
+    $nomeDoUsuario = 'joaodasilva123'; // É bom
+// Use nomes de variaveis que sejam curtos, mas ainda assim descritivos
+    $usuario = 'joao';
+// Evite abreviações que não sejam claras
+    $alturaCm = 175;
+
+/*
+Exemplos de camelCase:
+    - $nomeCompleto
+
+Exemplos de snake_case:
+    - $nome_completo
+
+Dica seja consistente com o estilo de nomenclatura que você escolher.
+    - Se você começar a usar camelCase, continue usando camelCase em todo o código.
+    - Se você começar a usar snake_case, continue usando snake_case em todo o código.
+    - Isso torna o código mais legível e fácil de entender.
+*/
+
+
+?>
