f8a9ba8f1a917bea21a2084dd6d176b79c4ccb19
Leonardo George <leonardogeorge30@gmail.com>
2025-05-10 09:42:12 -0300

    somaSubMultDiv

--- 05. Numeros/numerosInteiros.php
+++ 05. Numeros/numerosInteiros.php
@@ -2,8 +2,16 @@
 <?php
 
 $numero = 10; // Número inteiro
+$numero2 = 2; // Outro número inteiro
 
-echo "O meu numero inteiro é $numero <br/>";
+// Exibindo soma, subitração, divisão e multiplicação de dois números inteiros.
+echo "O meu número (1) inteiro é :"." ".$numero."<br/>";
+echo "O meu número (2) inteiro é :"." ".$numero2."<br/>";
+echo "<br/>";
 
 
+echo "O meu número inteiro (Soma) é :"." ".$numero+$numero2."<br/>";
+echo "O meu número inteiro (Subitração) é :"." ".$numero-$numero2."<br/>";
+echo "O meu número inteiro (Multiplicação) é :"." ".$numero*$numero2."<br/>";
+echo "O meu número inteiro (Divisão) é:"." ".$numero/$numero2."<br/>";
 ?>
